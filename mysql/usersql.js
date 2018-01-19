51359960fa547b2e607dc74189115ba9c7b3e1e3
guochao <347653397@qq.com>
2018-01-19 14:16:53 +0800

    eee

--- mysql/usersql.js
+++
@@ -1,6 +0,0 @@
-var UserSQL = {
-    insert: 'INSERT INTO tplay_admin(id,name) VALUES(?,?)',
-    queryAll: 'SELECT * FROM tplay_article',
-    getUserById: 'SELECT * FROM tplay_admin WHERE id = ? ',
-};
-module.exports = UserSQL;
