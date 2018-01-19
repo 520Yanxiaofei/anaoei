0efacf58421595e06082b74d849f991bd3fc8282
颜晓飞 <813377133@qq.com>
2018-01-19 15:01:45 +0800

    e

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ git pull origin master
 cd admin
 npm install
 pm2 delete all
-pm2 start ./bin/www --name anaoei-node-api
+pm2 start ./bin/www --name anaoei-node-api-pm2
 pm2 monit
 echo 嗨，anaoei,自动化部署成功！
