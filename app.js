fd6448995efc1b1c984a45afd4f5b2b7957e372e
guochao <347653397@qq.com>
2018-01-19 13:30:57 +0800

    eee

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//var proxy = require('http-proxy-middleware');//引入代理中间件
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var add_users = require('./routes/add_user');
+
+var app = express();
+
+
+//var apiProxy = proxy('/api', { target: 'http://101.132.144.219:3000',changeOrigin: true });//将服务器代理到localhost:8080端口上[本地服务器为localhost:3000]
+//app.use('/api/*', apiProxy);//api子目录下的都是用代理
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/add_users', add_users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ push.sh
@@ -0,0 +1,3 @@
+npm install
+pm2 start ./bin/www -name anaoei-node-api
+echo 嗨，anaoei,自动化部署成功！
